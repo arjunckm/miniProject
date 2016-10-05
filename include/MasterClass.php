e29301ab69fef9c6d2e88fa11b8379b8632eb951
arjunckm <arjunckm@yahoo.com>
2016-10-05 14:01:58 +0530

    Add files via upload
    new commit with visualization

--- include/MasterClass.php
+++ include/MasterClass.php
@@ -26,7 +26,7 @@ class MasterClass
     
     public function distinctState(){  //list of distinct state
 
-        $query = $this -> mysqli -> query("SELECT DISTINCT state FROM `students_details`");
+        $query = $this -> mysqli -> query("SELECT DISTINCT state FROM `students_details_me`");
         if ($this -> mysqli -> affected_rows > 0) {
             while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
                echo $row["state"]."<br/>";
@@ -39,7 +39,7 @@ class MasterClass
     public function distinctStatus(){ //distinct Status List
 
         $distinctStatusArr=array();
-        $query = $this -> mysqli -> query("SELECT DISTINCT status FROM `students_details`");
+        $query = $this -> mysqli -> query("SELECT DISTINCT status FROM `students_details_me`");
         if ($this -> mysqli -> affected_rows > 0) {
             while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
                $distinctStatusArr[]=$row['status'];
@@ -53,7 +53,7 @@ class MasterClass
         $statuslist=$func->distinctStatus();
         $sql="SELECT";
         foreach ($statuslist as $key => $value) {                      
-           $sql.="(SELECT COUNT(status) FROM `students_details` WHERE status='$value')as $value";
+           $sql.="(SELECT COUNT(status) FROM `students_details_me` WHERE status='$value')as $value";
            if($key==(count($statuslist)-1)){
                 $sql.="";
             }else{
@@ -63,7 +63,6 @@ class MasterClass
         $query = $this -> mysqli -> query("$sql");
         $dataArray=array();
         if ($this -> mysqli -> affected_rows > 0) {
-            $i=0;
             while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
                 $dataArray[]=$row;
             }
@@ -71,4 +70,74 @@ class MasterClass
         return json_encode($dataArray);
     }
 
+    public function getgenderRation(){
+        $query = $this -> mysqli -> query('select(SELECT count(gender) FROM `students_details_me` WHERE gender="M") as MALE, (SELECT count(gender) FROM `students_details_me` WHERE gender="F") as FEMALE');
+        $dataArray=array();
+        if ($this -> mysqli -> affected_rows > 0) {
+            while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
+                $dataArray[]=$row;
+            }
+        }
+        return json_encode($dataArray);
+    }
+
+    public function getallResultratio(){
+        $query = $this -> mysqli -> query('select(SELECT count(result) FROM `students_details_me` WHERE result="DIST") as Distinction, (SELECT count(result) FROM `students_details_me` WHERE result="FIRST") as First, (SELECT count(result) FROM `students_details_me` WHERE result="SECOND") as Second,(SELECT count(result) FROM `students_details_me` WHERE result="PASS") as JustPass');
+        $dataArray=array();
+        if ($this -> mysqli -> affected_rows > 0) {
+            while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
+                $dataArray[]=$row;
+            }
+        }
+        return json_encode($dataArray);
+    }
+
+
+    public function getDistinctStartYear(){ //distinct Start Year List
+
+        $DistinctStartYear=array();
+        $query = $this -> mysqli -> query("SELECT DISTINCT start_year FROM students_details_me ORDER by start_year ASC");
+        if ($this -> mysqli -> affected_rows > 0) {
+            while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
+               $DistinctStartYear[]=$row['start_year'];
+            }
+            return $DistinctStartYear;
+        }
+    }
+
+    public function getYearWiseEnrollment(){ //no. of Students Enrolled By Year
+        $func=new MasterClass();
+        $getlist=$func->getDistinctStartYear();
+
+        $sql="";
+        foreach ($getlist as $key => $value) {                      
+           $sql.="SELECT COUNT(status) as noofstudent FROM `students_details_me` WHERE start_year='$value'";
+           if($key==(count($getlist)-1)){
+                $sql.="";
+            }else{
+                $sql.=" UNION ";
+            }  
+        }
+        $query = $this -> mysqli -> query("$sql");
+        $dataArray=array();
+        if ($this -> mysqli -> affected_rows > 0) {
+            while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
+                $dataArray[]=$row;
+            }
+        }
+        return json_encode($dataArray);
+    }
+
+    public function getOddEvenEnrollRatio(){
+         $query = $this -> mysqli -> query('select(SELECT count(batch_type) FROM `students_details_me` WHERE batch_type="ODD") as ODD, (SELECT count(batch_type) FROM `students_details_me` WHERE batch_type="EVEN") as EVEN');
+        $dataArray=array();
+        if ($this -> mysqli -> affected_rows > 0) {
+            while ($row = $query -> fetch_array(MYSQLI_ASSOC)) {
+                $dataArray[]=$row;
+            }
+        }
+        return json_encode($dataArray);
+    }
+
+    
 }
--- index.php
+++ index.php
@@ -1,99 +1,210 @@
 <!DOCTYPE html>
-<meta charset="utf-8">
-
+<html>
 <head>
-	<style>
+<meta charset=utf-8> <meta content="IE=edge" http-equiv=X-UA-Compatible> <meta content="width=device-width,initial-scale=1" name=viewport>
+  <title>Visualization</title>
+  <link rel="stylesheet" type="text/css" href="css/c3.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-material-design.css">
+  <link rel="stylesheet" type="text/css" href="css/ripples.css">
 
-	.axis {
-	  font: 10px sans-serif;
-	}
+  <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>  
+  <script type="text/javascript" src="js/d3.v3.min.js"></script>
+  <script type="text/javascript" src="js/c3.min.js"></script>
+  <script type="text/javascript" src="js/material.js"></script>
+  <script type="text/javascript" src="js/ripples.js"></script>
 
-	.axis path,
-	.axis line {
-	  fill: none;
-	  stroke: #000;
-	  shape-rendering: crispEdges;
-	}
-
-	</style>
 </head>
-
 <body>
-	
-<script src="http://d3js.org/d3.v3.min.js"></script>
+<div class="container">
+<div class="col-md-12" style="top:15px">
+  <div class="col-md-3" style="top: 19px;">
+    <img src="images/logo.png">
+  </div>
+  <div class="col-md-8">
+  <h3 style="text-align:center">School of Information Sciences</h3>
+  <h4 style="text-align:center">Data Analysis & Visualization - Students Academic Performance Analysis </h4>
+  </div>
+</div>
+<div class="col-md-12">
+<div class="form-inline">
+  <div class="form-group label-floating">
+      <label for="listofdata" class="">Select Data List</label><br/>
+      <select id="listofdata" class="form-control ">
+        <option value="">Select Option</option>
+        <option value="status">Status</option>
+        <option value="genderratio">Gender Ratio</option>
+        <option value="resultall">All Result (Final Totals)</option>
+        <option value="oddevenenroll">Odd/Even Sem Enroll Ratio</option>
+        <option value="yearenroll">Student Enrollment by Year</option>        
+      </select>
+  </div>
+  <div class="form-group" style="margin-left:20px">
+      <label>Select Chart type</label><br/>
+      <button class="btn btn-raised btn-primary dataType" value="line">Line Chart</button>
+      <button class="btn btn-raised btn-success dataType" value="bar">Bar Chart</button>
+      <button class="btn btn-raised btn-info dataType" value="scatter">Scatter Chart</button>
+      <button class="btn btn-raised btn-danger dataType" value="pie">Pie Chart</button>
+      <button class="btn btn-raised btn-warning dataType" value="donut">Donut Chart</button>
+      <button class="btn btn-raised btn-primary dataType" value="gauge">Gauge Chart</button>
+      <!-- <input type="radio" class="dataType" name="dataType" value="line"/>Line <br/>
+      <input type="radio" class="dataType" name="dataType" value="spline"/>spline
+      <input type="radio" class="dataType" name="dataType" value="step"/>step
+      <input type="radio" class="dataType" name="dataType" value="area"/>area
+      <input type="radio" class="dataType" name="dataType" value="area-spline"/>area-spline
+      <input type="radio" class="dataType" name="dataType" value="area-step"/>area-step
+      <input type="radio" class="dataType" name="dataType" value="bar"/>Bar Graph<br/>
+      <input type="radio" class="dataType" name="dataType" value="scatter"/>Scatter View<br/>
+      <input type="radio" class="dataType" name="dataType" value="pie"/>Pie Chart<br/>
+      <input type="radio" class="dataType" name="dataType" value="donut"/>Donut Look<br/>
+      <input type="radio" class="dataType" name="dataType" value="gauge"/>Gauge<br/> -->
+  </div>
+  
+</div>
+</div>
+<div class="col-md-12">
+  
+<div id="chart"></div>
 
-<script>
+</div>
+<script type="text/javascript">
+$(".dataType").click(function(){
+  var datalist=$("#listofdata").val();
+  dataType=$(this).val();
 
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+  if(datalist==""){
+      $("#chart").html('<span class="alert alert-warning">Please Select Data List</span>');
+  }else if(dataType==undefined){
+    $("#chart").html('<span class="alert alert-warning">Please Select Chart Type</span>');
+  }else{
 
-// Parse the date / time
-var	parseDate = d3.time.format("%Y-%m").parse;
+    if(datalist=="status"){
+      d3.json("response.php?data=status", function(dataRes) {
+         data = {};
+         val=[];
+        dataRes.forEach(function(e) {
+            val.push('COMPLETED');
+            val.push('INCOMPLETE');
+            val.push('EXPIRED');
+            data['COMPLETED'] = e.COMPLETED;
+            data['INCOMPLETE'] = e.INCOMPLETE;
+            data['EXPIRED'] = e.EXPIRED;
+        });
+        var chart = c3.generate({
+          data: {
+              json: [ data ],
+              keys: {
+                  value: val,
+              },
+              type: dataType
+          }
+          });
+      });
+  }else if(datalist=="genderratio"){
+    d3.json("response.php?data=genderratio", function(dataRes) {
+         data = {};
+         val=[];
+        dataRes.forEach(function(e) {
+            val.push('MALE');
+            val.push('FEMALE');
+            data['MALE'] = e.MALE;
+            data['FEMALE'] = e.FEMALE;
+        });
+        var chart = c3.generate({
+          data: {
+              json: [ data ],
+              keys: {
+                  value: val,
+              },
+              type: dataType
+          }
+          });
+      });
+  }else if(datalist=="resultall"){
+    d3.json("response.php?data=resultall", function(dataRes) {
+         data = {};
+         val=[];
+        dataRes.forEach(function(e) {
+            val.push('Distinction');
+            val.push('First');
+            val.push('Second');
+            val.push('Just Pass');
+            data['Distinction'] = e.Distinction;
+            data['First'] = e.First;
+            data['Second'] = e.Second;
+            data['Just Pass'] = e.JustPass;
+        });
+        console.log(data);
+        var chart = c3.generate({
+          data: {
+              json: [ data ],
+              keys: {
+                  value: val,
+              },
+              type: dataType
+          }
+          });
+      });
+  }else if(datalist=="oddevenenroll"){
+    d3.json("response.php?data=oddevenEnroll", function(dataRes) {
+         data = {};
+         val=[];
+        dataRes.forEach(function(e) {
+            val.push('ODD');
+            val.push('EVEN');
+            data['ODD'] = e.ODD;
+            data['EVEN'] = e.EVEN;
+        });
+        console.log(data);
+        var chart = c3.generate({
+          data: {
+              json: [ data ],
+              keys: {
+                  value: val,
+              },
+              type: dataType
+          }
+          });
+      });
+  }else if(datalist=="yearenroll"){
+    d3.json("response.php?data=yearenroll", function(dataRes) {
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+         data= {};
+         val=[];
+         newarr=[];
+         console.log(dataRes);
 
-var y = d3.scale.linear().range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m"));
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-
-d3.csv("bar-data.csv", function(error, data) {
-
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.value = +d.value;
-    });
-	
-  x.domain(data.map(function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Value ($)");
-
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.date); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
-
+        dataRes.forEach(function(e) {
+            val.push('No. of Students');
+            data['noofstudent']=e.noofstudent;
+            newarr.push(data);
+        });
+        console.log(data);
+        var chart = c3.generate({
+          data: {
+              json: [ newarr ],
+              keys: {
+                  value: val,
+              },
+              type: dataType
+          }
+          });
+      });
+  }
+}
 });
 
+  
+
 </script>
+<hr/>
+<footer class="navbar-fixed-bottom" style="padding: 19px;background: black;color: aliceblue;">
+  <div class="col-md-12"><span>Semester Project - Monsoon 2016 | Executed by Arjun JS & Chethan GS</span>
+  <span class="pull-right">Github Repo Link: <a href="https://github.com/arjunckm">https://github.com/arjunckm</a></span>
+  </div>
+</footer>
+</div>
 
 </body>
 </html>
